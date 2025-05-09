9a9f6f3182ac95e3dccba5f565b83f0d6197b3f4
Nick Gondek <npg@berkeley.edu>
2025-05-09 17:49:56 +0000

    Generate test points 1:1 for external netcdfs

--- exploratory/download_caladapt.py
+++ exploratory/download_caladapt.py
@@ -12,18 +12,22 @@ def main():
         ## Sum of precipitation
         ("Riverside County", "Precipitation (total)", "sum", "precip_riverside_annual"),
         ("San Bernardino County", "Precipitation (total)", "sum", "precip_sanbernardino_annual"),
+        ("Tulare County", "Precipitation (total)", "sum", "precip_tulare_annual"),
 
         ## Maximum temperature of maximums
         ("Riverside County", "Maximum air temperature at 2m", "max", "maxtemp_riverside_annual"),
         ("San Bernardino County", "Maximum air temperature at 2m", "max", "maxtemp_sanbernardino_annual"),
+        ("Tulare County", "Maximum air temperature at 2m", "max", "maxtemp_tulare_annual"),
 
         ## Minimum temperature of mininums
         ("Riverside County", "Minimum air temperature at 2m", "min", "mintemp_riverside_annual"),
         ("San Bernardino County", "Minimum air temperature at 2m", "min", "mintemp_sanbernardino_annual"),
+        ("Tulare County", "Minimum air temperature at 2m", "min", "mintemp_tulare_annual"),
 
         ## Mean temperature (of maximum, since we don't have mean directly)
         ("Riverside County", "Maximum air temperature at 2m", "mean", "meantemp_riverside_annual"),
         ("San Bernardino County", "Maximum air temperature at 2m", "mean", "meantemp_sanbernardino_annual"),
+        ("Tulare County", "Maximum air temperature at 2m", "mean", "meantemp_tulare_annual"),
     ]
     
     # Process all combinations
@@ -37,7 +41,8 @@ def main():
             simulation_name=SIMULATION,
             warming_level=WARMING_LEVEL,
             aggregation_method=aggregation,
-            output_path=output_path
+            output_path=output_path,
+            generate_test_points=True,
         )
         
         print(f"Data saved to {output_path}")
